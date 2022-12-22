0c07a70061bab86ab4110fd516b120c75258bdb7
Taboada, Emmanuel <89477898+emmanueltab@users.noreply.github.com>
2022-12-22 00:56:30 -0600

    player movement kinda done

--- WeepingSouls/player.gd
+++ WeepingSouls/player.gd
@@ -1,17 +1,26 @@
 extends KinematicBody2D
 
+const ACCELERATION = 500
+const MAX_SPEED = 200
+const FRICTION = 500
 
-export(float) var movement_speed = 200
+var velocity = Vector2.ZERO
+
+
 
 func _physics_process(delta): # basic wasd movement. diagonal movement is faster
 	
-	var directional_input = Vector2.ZERO
+	var input_vector = Vector2.ZERO
 	
-	directional_input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	directional_input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
-	move_and_slide(directional_input * movement_speed)
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move_and_slide(velocity)
 
-func _ready():
-	pass # Replace with function body.
 
