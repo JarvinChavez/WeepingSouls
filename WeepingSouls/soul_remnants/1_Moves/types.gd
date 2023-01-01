2b45ac53435fde24b6ac2cefbfdf781cbce26654
Taboada, Emmanuel <89477898+emmanueltab@users.noreply.github.com>
2022-12-31 22:28:50 -0600

    made more changes to type advantages

--- WeepingSouls/soul_remnants/1_Moves/types.gd
+++ WeepingSouls/soul_remnants/1_Moves/types.gd
@@ -1,9 +1,6 @@
 extends Node
 func damage_after_types(Raw_Damage, attackMoveType: MoveStats, defenseType1: SRstats, defenseType2: SRstats) -> int:
-	
-	var Move_type = attackMoveType.type # attack
-	var onDefense_type1 = defenseType1.type # defense
-	var onDefense_type2 = defenseType2.type
+
 	var TMC_dam = 0
 	
 					# defend:
@@ -35,10 +32,10 @@ func damage_after_types(Raw_Damage, attackMoveType: MoveStats, defenseType1: SRs
 							[1.0,    0.5,    0.5,     2.0,    1.0,   2.0,    1.0,   1.0,      0.5,     0.5,       1.0,       1.0,   0.0] # air
 						]
 						
-	TMC_dam = Raw_Damage * type_advantages[Move_type][onDefense_type1] # first check
+	TMC_dam = Raw_Damage * type_advantages[attackMoveType][defenseType1] # first check
 	
-	if defenseType2.type != SRstats.none:
-		TMC_dam = TMC_dam * type_advantages[Move_type][onDefense_type2] # second check if the defending pokemon has 2 types
+	if defenseType2 != SRstats.none:
+		TMC_dam = TMC_dam * type_advantages[attackMoveType][defenseType2] # second check if the defending pokemon has 2 types
 	
 	return TMC_dam
 	
