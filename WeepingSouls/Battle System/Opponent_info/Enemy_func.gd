236ddef86cae5e21f2d3a409b9bcb7ae78a2b352
Jarvin Chavez <112718763+JarvinChavez@users.noreply.github.com>
2023-01-02 21:17:24 -0600

    npcs working. lesgo

--- WeepingSouls/Battle System/Opponent_info/Enemy_func.gd
+++ WeepingSouls/Battle System/Opponent_info/Enemy_func.gd
@@ -1,21 +1,81 @@
-extends Position2D
+extends KinematicBody2D
 
-onready var SRstats = find_node("SR_stats")
-onready var N_pc = find_node("N_pc") setget set_N_pc, get_N_pc
-var SoulRemant setget set_SoulRemnant, get_SoulRemnant
+#NPC info
 
-func set_SoulRemnant(value):
-	if get_SoulRemnant():
-		get_SoulRemnant().free()
-	find_node("SoulRemnant").add_child(value.instance())
 
-func get_SoulRemnant() -> Node:
-	if find_node("SoulRemnant").get_child_count() > 0:
-		return find_node("SoulRemnant").get_child(0)
-	return null
+# trigger
 
-func set_N_pc(value):
-	N_pc = value
 
-func get_N_pc():
-	return get_node_or_null("N_pc")
+
+# states of npc
+export(int) var speed: int = 70
+export(bool) var active
+export(float, 0, 1.0) var Trigger_chance
+export (String, "loop", "linear") var walk_method = "linear"
+var state: String = "patrol"
+var direction = 1
+var in_sight: bool = false
+
+onready var insight_area = $Noticed_Area
+onready var pathfollow = get_parent()
+onready var Player = get_node("../../../player")
+
+func _patrol(delta):
+	if walk_method == "loop":
+		pathfollow.offset += speed * delta
+		if rotation == 0:
+			rotation_degrees = lerp(rotation_degrees, 0, 0.2)
+	else:
+		if direction == 1:
+			if pathfollow.unit_offset == 1:
+				yield(get_tree().create_timer(0.3), "timeout")
+				rotation_degrees = lerp(rotation_degrees, 180, 0.1)
+				yield(get_tree().create_timer(1), "timeout")
+				direction = 0
+			else:
+				pathfollow.offset += speed * delta
+		else:
+			if pathfollow.unit_offset == 0:
+				yield(get_tree().create_timer(0.3), "timeout")
+				rotation_degrees = lerp(rotation_degrees, 0, 0.1)
+				yield(get_tree().create_timer(1), "timeout")
+				direction = 1
+			else:
+				pathfollow.offset -= speed * delta
+
+func _physics_process(delta: float) -> void:
+		if in_sight:
+			_insight()
+		if state == "patrol":
+			_patrol(delta)
+			
+
+
+func _insight() -> void:
+	var area = get_world_2d().direct_space_state
+	var result = area.intersect_ray(global_position, Player.global_position, [self], collision_mask)
+	if result:
+		if result.collider.name == "player" :
+			state = "fight"
+			look_at(Player.global_position)
+	
+
+
+func _on_NPC_body_entered(_body_rid, body: Node, _body_shape_index, _local_shape_index):
+	if body.name == "player":
+		state = "fight"
+		in_sight = true
+		if randf() <= Trigger_chance:
+			if active:
+				$noticed.visible = in_sight
+				Player.control_lock = true
+				var inbattle = preload("res://Battle System/NPcvsBattle.tscn").instance()
+				$"../../../BattleStart".add_child(inbattle)
+		else:
+			return
+
+func  _on_NPC_body_exited(_body_rid, body: Node, _body_shape_index, _local_shape_index):
+	if body.name == "player":
+		in_sight = false
+		state = "patrol"
+		$noticed.visible = in_sight
--- WeepingSouls/scenes/characters/basePlayer.gd
+++ WeepingSouls/scenes/characters/basePlayer.gd
@@ -5,21 +5,22 @@ const MAX_SPEED = 200
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
-
+var control_lock = false
 
 func _physics_process(delta): # basic wasd movement. diagonal movement is faster
 	
-	var input_vector = Vector2.ZERO
-	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	move_and_slide(velocity)
+	if not control_lock:
+		var input_vector = Vector2.ZERO
+		
+		input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		input_vector = input_vector.normalized()
+		
+		if input_vector != Vector2.ZERO:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+		move_and_slide(velocity, Vector2(0,0), false, 4, 0.785, false)
 
 
