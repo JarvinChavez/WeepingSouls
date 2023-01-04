d355f8c162a5fd1ff3c384354da3bef30b748ac7
Taboada, Emmanuel <89477898+emmanueltab@users.noreply.github.com>
2023-01-04 01:46:55 -0600

    worked on pause menu

--- WeepingSouls/scenes/areas/baseArea_script.gd
+++ WeepingSouls/scenes/areas/baseArea_script.gd
@@ -3,6 +3,9 @@ extends Node2D
 onready var tween_:Tweenkies = $tween
 onready var info_box_ = $Dialoguebox
 
+#constants:
+const PauseScreen = preload("res://scenes/menus/pauseMenu/pauseScreen.tscn")
+
 func floor_vec2(vector:Vector2) -> Vector2:
 	return Vector2(floor(vector.x / 16.0) * 16.0, floor(vector.y / 16.0) * 16.0)
 
@@ -22,3 +25,10 @@ func _on_encounter(trainer) -> void:
 	battle.queue_free()
 	
 	trainer.emit_signal("beat")
+	
+	
+# for the pause screen:
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		var pause_menu = PauseScreen.instance()
+		add_child(pause_menu)
